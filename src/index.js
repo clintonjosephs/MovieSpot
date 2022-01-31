9cba9eeb3b8fa7ac1d9095bd7f30ffcc8af74025
clintonjosephs <clintonjosephs26@gmail.com>
2022-01-31 15:36:38 +0100

    fixed linters errors

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import './style.css';
 import _ from 'lodash';
 
 function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'world'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  const element = document.createElement('div');
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'world'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
