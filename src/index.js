bea7ad68bd53fdc389130e55588be8605bbfc143
clintonjosephs <clintonjosephs26@gmail.com>
2022-01-31 15:34:43 +0100

    add to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './style.css';
+import _ from 'lodash';
+
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'world'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
