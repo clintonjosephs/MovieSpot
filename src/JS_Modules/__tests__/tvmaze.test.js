45ebac655090d787a9d2d71175addb097b753693
clintonjosephs <clintonjosephs26@gmail.com>
2022-02-01 23:42:28 +0100

    fixed linters errors

--- src/JS_Modules/__tests__/tvmaze.test.js
+++ src/JS_Modules/__tests__/tvmaze.test.js
@@ -1,21 +1,21 @@
-import Count from "../Controllers/Count.js";
-import movies from "../__mocks__/moviesMock.js";
+import Count from '../Controllers/Count.js';
+import movies from '../__mocks__/moviesMock.js';
 
 describe('check count of items to be displayed on the homepage', () => {
-    test('test if count function returns correct count', () => {
-         // Arrange
-         const data = movies;
+  test('test if count function returns correct count', () => {
+    // Arrange
+    const data = movies;
 
-         //Act
-         const count = Count(data);
+    // Act
+    const count = Count(data);
 
-         //Assert
+    // Assert
 
-         expect(count).toEqual(10);
-         expect(count).toEqual(data.length);
-         expect(count - data.length).toEqual(0);
-         expect(count - 10).toEqual(0);
-         expect(typeof data).toEqual('object');
-         expect(typeof count).toEqual('number');
-    });
+    expect(count).toEqual(10);
+    expect(count).toEqual(data.length);
+    expect(count - data.length).toEqual(0);
+    expect(count - 10).toEqual(0);
+    expect(typeof data).toEqual('object');
+    expect(typeof count).toEqual('number');
+  });
 });
