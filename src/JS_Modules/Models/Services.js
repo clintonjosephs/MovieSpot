c42cc826a5c75d3061aed00b1e37dba422a17c0f
ahmed tawfik ali <a.alrakhawy@gmail.com>
2022-02-01 10:48:10 +0200

    Edit modalHandler.js,Services.js and Utils.js

--- src/JS_Modules/Models/Services.js
+++ src/JS_Modules/Models/Services.js
@@ -1,1 +1,8 @@
-// import BaseUrl from './Utils.js';
+import { BaseUrl, getSingleMovieEndPoint } from './Utils.js';
+
+const getMoviePopupDetails = async (movieID) => {
+  const response = await fetch(`${BaseUrl}${getSingleMovieEndPoint}${movieID}`);
+  return response.json();
+};
+
+export default getMoviePopupDetails;
