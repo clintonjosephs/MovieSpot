966e725a3ab3dd76aba318c5cfce474c4f9f21f0
clintonjosephs <clintonjosephs26@gmail.com>
2022-02-02 13:03:09 +0100

    preparing for gh-pages

--- src/JS_Modules/Models/Utils.js
+++ src/JS_Modules/Models/Utils.js
@@ -2,7 +2,7 @@ const BaseUrl = 'https://api.tvmaze.com/';
 const contentType = { 'content-type': 'application/json' };
 const getMoviesEndPoint = 'search/shows';
 const getSingleMovieEndPoint = 'shows/';
-const involvmentApiEndPoint = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/W9LtdOzBoZfxFshRZlqy/';
+const involvmentApiEndPoint = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/EfW4baGDbzq78AaqqcR8/';
 
 const dateFormatter = (date) => {
   const d = new Date(date);
