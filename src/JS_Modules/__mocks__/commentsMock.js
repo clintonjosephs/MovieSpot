fa01274340862b534f13b093218f233ba0bcea20
ahmed tawfik ali <a.alrakhawy@gmail.com>
2022-02-02 13:25:37 +0200

    Created commentsMock.js

---
+++ src/JS_Modules/__mocks__/commentsMock.js
@@ -0,0 +1,7 @@
+const comments = [
+  { username: 'clinton', creation_date: '2022-02-01', comment: 'Nice movie' },
+  { comment: 'ahmed', creation_date: '2022-02-01', username: 'very scary' },
+  { comment: 'ammar', creation_date: '2022-02-01', username: 'i like it' },
+];
+
+export default comments;
--- src/JS_Modules/__tests__/tvmaze.test.js
+++ src/JS_Modules/__tests__/tvmaze.test.js
@@ -1,5 +1,6 @@
 import Count from '../Controllers/Count.js';
 import movies from '../__mocks__/moviesMock.js';
+import comments from '../__mocks__/commentsMock.js';
 
 describe('check count of items to be displayed on the homepage', () => {
   test('test if count function returns correct count', () => {
@@ -15,6 +16,25 @@ describe('check count of items to be displayed on the homepage', () => {
     expect(count).toEqual(data.length);
     expect(count - data.length).toEqual(0);
     expect(count - 10).toEqual(0);
+    expect(typeof data).toEqual('object');
+    expect(typeof count).toEqual('number');
+  });
+});
+
+describe('check count of comments to be displayed on the modal popup', () => {
+  test('test if count function returns correct comment count', () => {
+    // Arrange
+    const data = comments;
+
+    // Act
+    const count = Count(data);
+
+    // Assert
+
+    expect(count).toEqual(3);
+    expect(count).toEqual(data.length);
+    expect(count - data.length).toEqual(0);
+    expect(count - 3).toEqual(0);
     expect(typeof data).toEqual('object');
     expect(typeof count).toEqual('number');
   });
